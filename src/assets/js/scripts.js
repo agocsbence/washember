acc1282903c03e85fa0bcb4368aca3460a7fbd41
Bence Agocs <bence@harom.studio>
2020-10-06 22:17:27 +0200

    content update 1

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -2,18 +2,17 @@
 const images = ["assets/img/spray.png", "assets/img/telephone.png", "assets/img/tool-1.png", "assets/img/tool-2.png", "assets/img/tool-3.png", "assets/img/tool-4.png", "assets/img/tool-5.png"],
       random = Math.floor(Math.random() * images.length);
 
-let bgImg1 = document.getElementById('img1'),
-    bgImg2 = document.getElementById('img2');
+// let bgImg1 = document.getElementById('img1'),
+//     bgImg2 = document.getElementById('img2');
 
-var posx = Math.floor(Math.random() * 30),
-    posy = Math.floor(Math.random() * 40);
+// var posx = Math.floor(Math.random() * 30),
+//     posy = Math.floor(Math.random() * 40);
 
 window.onload = (event) => {
-    console.log(posx);
-    console.log(posy);
     bgImg1.src = images[random];
-    bgImg1.style.top = posy + '%';
-    bgImg1.style.left = posx + '%';
+    bgImg2.src = images[random];
+    // bgImg1.style.top = posy + '%';
+    // bgImg1.style.left = posx + '%';
 };
 
 

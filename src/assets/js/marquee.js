c6e8eec2f05642d959318fc6f4f74e8b54d6e57c
Bence Agocs <bence@harom.studio>
2020-10-08 20:47:49 +0200

    footer marquee finished

--- src/assets/js/marquee.js
+++ src/assets/js/marquee.js
@@ -67,21 +67,3 @@ var Marquee = Class.create({
     Event.observe(document, 'dom:loaded', function() {
     $$('.marquee').each(function(e) {new Marquee(e)})
     });
-//change bgImages
-const images = ["assets/img/spray.png", "assets/img/telephone.png", "assets/img/tool-1.png", "assets/img/tool-2.png", "assets/img/tool-3.png", "assets/img/tool-4.png", "assets/img/tool-5.png"],
-      random = Math.floor(Math.random() * images.length);
-
-// let bgImg1 = document.getElementById('img1'),
-//     bgImg2 = document.getElementById('img2');
-
-// var posx = Math.floor(Math.random() * 30),
-//     posy = Math.floor(Math.random() * 40);
-
-// window.onload = (event) => {
-//     bgImg1.src = images[random];
-//     bgImg2.src = images[random];
-//     bgImg1.style.top = posy + '%';
-//     bgImg1.style.left = posx + '%';
-// };
-
-

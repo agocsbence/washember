fa2199775a24dc3c176aa469411543299fa3c944
Bence Agocs <bence@harom.studio>
2020-10-13 01:11:19 +0200

    acf options page added to functions.php

--- src/functions.php
+++ src/functions.php
@@ -4,3 +4,16 @@ function no_wordpress_errors() {
 	return 'Oops! Something is wrong!';
 }
 add_filter('login_errors', 'no_wordpress_errors');
+
+/* option page */
+if( function_exists('acf_add_options_page') ) {
+	
+	acf_add_options_page(array(
+		'page_title' 	=> 'Egyedi mezők',
+		'menu_title'	=> 'Washember',
+		'menu_slug' 	=> 'washember-general-settings',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false
+	));
+	
+}

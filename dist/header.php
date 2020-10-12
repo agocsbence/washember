581d9c9d82b8bc299d0a5954839360ff0652fac3
Bence Agocs <bence@harom.studio>
2020-10-13 01:44:26 +0200

    user id

--- dist/header.php
+++ dist/header.php
@@ -14,23 +14,13 @@
 </head>
 <body>
 
-<div class="userinfo">
-
-<?php $user_info = get_userdata(9);
-      echo 'Username: ' . $user_info->user_login . "\n";
-      echo 'User roles: ' . implode(', ', $user_info->roles) . "\n";
-      echo 'User ID: ' . $user_info->ID . "\n";
-?>
-
-</div>
-
 <header class="bg--white grid grid--header">
     <img src="<?php bloginfo('template_url') ?>/assets/img/logo-desktop.png" alt="Washember" class="logo desktop-only">
     <img src="<?php bloginfo('template_url') ?>/assets/img/logo-mobile.png" alt="Washember" class="logo-mobile mobile-only">
     <p><strong>washember</strong> <span class="desktop-only">|</span> javít a szitun <span class="desktop-only">|</span></p>
     <div class="marquee-header">
-        <div class="marquee1">ha sürgős hívj <a href="tel:" target="_blank" rel="noopener noreferrer"><img src="<?php bloginfo('template_url') ?>/assets/img/phone.png" alt="phone icon"></a> és repülünk <a href="tel:+36301234567">+36 30 123 4567</a></div>
-        <div class="marquee2">ha sürgős hívj <a href="tel:" target="_blank" rel="noopener noreferrer"><img src="<?php bloginfo('template_url') ?>/assets/img/phone.png" alt="phone icon"></a> és repülünk <a href="tel:+36301234567">+36 30 123 4567</a></div>
-        <div class="marquee3">ha sürgős hívj <a href="tel:" target="_blank" rel="noopener noreferrer"><img src="<?php bloginfo('template_url') ?>/assets/img/phone.png" alt="phone icon"></a> és repülünk <a href="tel:+36301234567">+36 30 123 4567</a></div>
+        <div class="marquee1">ha sürgős hívj <a href="tel:<?php the_field('telefonszam', 'option'); ?>" target="_blank" rel="noopener noreferrer"><img src="<?php bloginfo('template_url') ?>/assets/img/phone.png" alt="phone icon"></a> és repülünk <a href="tel:<?php the_field('telefonszam', 'option'); ?>"><?php the_field('telefonszam', 'option'); ?></a></div>
+        <div class="marquee2">ha sürgős hívj <a href="tel:<?php the_field('telefonszam', 'option'); ?>" target="_blank" rel="noopener noreferrer"><img src="<?php bloginfo('template_url') ?>/assets/img/phone.png" alt="phone icon"></a> és repülünk <a href="tel:<?php the_field('telefonszam', 'option'); ?>"><?php the_field('telefonszam', 'option'); ?></a></div>
+        <div class="marquee3">ha sürgős hívj <a href="tel:<?php the_field('telefonszam', 'option'); ?>" target="_blank" rel="noopener noreferrer"><img src="<?php bloginfo('template_url') ?>/assets/img/phone.png" alt="phone icon"></a> és repülünk <a href="tel:<?php the_field('telefonszam', 'option'); ?>"><?php the_field('telefonszam', 'option'); ?></a></div>
     </div>
 </header>

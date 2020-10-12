487417e075224b7ce2ff752c03e8e65fe38c2a9c
Bence Agocs <bence@harom.studio>
2020-10-13 01:19:40 +0200

    remove menu items in admin

--- src/functions.php
+++ src/functions.php
@@ -15,5 +15,29 @@ if( function_exists('acf_add_options_page') ) {
 		'capability'	=> 'edit_posts',
 		'redirect'		=> false
 	));
-	
+
+}
+
+/* remove admin menu items based on user ID */
+add_action( 'admin_init', 'my_remove_menu_pages' );
+function my_remove_menu_pages() {
+
+
+  global $user_ID;
+
+  if ( $user_ID != 2 ) { //your user id
+
+   remove_menu_page('edit.php'); // Posts
+   remove_menu_page('upload.php'); // Media
+   remove_menu_page('link-manager.php'); // Links
+   remove_menu_page('edit-comments.php'); // Comments
+   remove_menu_page('edit.php?post_type=page'); // Pages
+   remove_menu_page('plugins.php'); // Plugins
+   remove_menu_page('themes.php'); // Appearance
+   remove_menu_page('users.php'); // Users
+   remove_menu_page('tools.php'); // Tools
+   remove_menu_page('options-general.php'); // Settings
+   remove_menu_page('edit.php'); // Posts
+   remove_menu_page('upload.php'); // Media
+  }
 }

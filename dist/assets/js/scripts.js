0ba92ae2335de73a38ab4f50e0d0a137665ddf31
Bence Agocs <bence@harom.studio>
2020-10-13 00:57:54 +0200

    css bugs fixed

--- dist/assets/js/scripts.js
+++ dist/assets/js/scripts.js
@@ -71,17 +71,16 @@ var Marquee = Class.create({
 const images = ["assets/img/spray.png", "assets/img/telephone.png", "assets/img/tool-1.png", "assets/img/tool-2.png", "assets/img/tool-3.png", "assets/img/tool-4.png", "assets/img/tool-5.png"],
       random = Math.floor(Math.random() * images.length);
 
-// let bgImg1 = document.getElementById('img1'),
-//     bgImg2 = document.getElementById('img2');
+let bgImg1 = document.getElementById('img1'),
+    content = document.getElementById('content');
 
-// var posx = Math.floor(Math.random() * 30),
-//     posy = Math.floor(Math.random() * 40);
+window.addEventListener('load', setImageSize);
+window.addEventListener('resize', setImageSize);
 
-// window.onload = (event) => {
-//     bgImg1.src = images[random];
-//     bgImg2.src = images[random];
-//     bgImg1.style.top = posy + '%';
-//     bgImg1.style.left = posx + '%';
-// };
+console.log(bgImg1);
+console.log(content);
+console.log(content.clientHeight);
 
-
+function setImageSize() {
+    bgImg1.style.maxHeight = (content.clientHeight * 0.9) + "px";
+}

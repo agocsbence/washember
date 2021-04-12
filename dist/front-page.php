e71774ff49f622dd3d587e1d207cdc6fefa22a38
Bence Agocs <bence@harom.studio>
2021-04-12 13:48:03 +0200

    home page updated

--- dist/front-page.php
+++ dist/front-page.php
@@ -9,35 +9,15 @@ get_header();
 
 ?>
 
-<main class="grid grid--main">
-    <div id="bgImages">
-        <div class="wrapper">
-            <img src="<?php bloginfo('template_url') ?>/assets/img/img1.png" alt="" id="img1">
-            <img src="<?php bloginfo('template_url') ?>/assets/img/img2.png" alt="" id="img2">
-        </div>
+<main class="" id="home-page-content">
+    <div id="lakossagi" class="circle">
+        <p>lakossági</p>
+        <p>+36 20 266 3604</p>
     </div>
-    <section id="content">
-        <div class="text">
-            <p>Ha elakadtál otthon, irodában, üzletben valami javításban, valami <u>elromlott, leromlott, koszos lett, kiömlött,</u> vagy <u>eldugult, leszakadt, szétesett,</u> keress minket, megnézzük, mit tehetünk.</p>
-            <p>Nagy valószínűséggel meg tudjuk javítani, fel tudjuk újítani, ki tudjuk tisztítani, tudunk segíteni!</p>
-        </div>
-    </section>
-    <section id="form">
-        <p>Írj, miben segíthetünk</p>
-        <div id="thankyou">
-            <p>Köszönjük az üzenetet, hamarosan jelentkezünk!</p>
-            <div class="button" id="tyButton">Újat írok</div>
-        </div>
-        <?php echo do_shortcode('[contact-form-7 id="11" title="Üzenet"]'); ?>
-    </section>
-    <section id="contact">
-        <p>Ha sürgős, <u>hívj</u>, és <u>repülünk</u></p>
-        <p><a href="tel:<?php the_field('telefonszam', 'option'); ?>"><?php the_field('telefonszam', 'option'); ?></a></p>
-        <div class="copyright">
-            <p><small>Oldalunk óriási fejlesztés előtt áll.</small></p>
-            <p><small>2020 &copy; Golem Holding</small></p>
-        </div>
-    </section>
+    <div id="uzleti" class="circle">
+        <p>üzleti</p>
+        <p>+36 30 890 2585</p>
+    </div>
 </main>
 
 <?php get_footer(); ?>

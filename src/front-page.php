f28fe7a77bf9707395549a8ad658060860895f71
Bence Agocs <bence@harom.studio>
2021-04-12 14:26:07 +0200

    home page updated

--- src/front-page.php
+++ src/front-page.php
@@ -10,14 +10,14 @@ get_header();
 ?>
 
 <main class="" id="home-page-content">
-    <a href="" id="lakossagi" class="circle">
+    <div onclick="location.href = 'www.yoursite.com';" id="lakossagi" class="circle">
         <p><strong>lakossági</strong></p>
         <p><strong><a href="tel:+36 20 266 3604">+36 20 266 3604</a></strong></p>
-    </a>
-    <a href="" id="uzleti" class="circle">
+    </div>
+    <div onclick="location.href = 'www.yoursite.com';" id="uzleti" class="circle">
         <p><strong>üzleti</strong></p>
         <p><strong><a href="tel:+36 30 890 2585">+36 30 890 2585</a></strong></p>
-    </a>
+    </div>
 </main>
 
 <?php get_footer(); ?>

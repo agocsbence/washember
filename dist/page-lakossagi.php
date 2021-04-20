384b6c4701bff7c282a8f10b7677284998eb13ab
Bence Agocs <bence@harom.studio>
2021-04-20 09:17:33 +0200

    uzleti updated

--- dist/page-lakossagi.php
+++ dist/page-lakossagi.php
@@ -18,22 +18,22 @@ get_header();
     </div>
     <section id="content">
         <div class="text">
-            <p>Ha elakadtál otthon, irodában, üzletben valami javításban, valami <u>elromlott, leromlott, koszos lett, kiömlött,</u> vagy <u>eldugult, leszakadt, szétesett,</u> keress minket, megnézzük, mit tehetünk.</p>
+            <p>Ha elakadt otthon valami javításban, valami <u>elromlott, leromlott, koszos lett, kiömlött,</u> vagy <u>eldugult, leszakadt, szétesett,</u> keressen minket, megnézzük, mit tehetünk!</p>
             <p>Nagy valószínűséggel meg tudjuk javítani, fel tudjuk újítani, ki tudjuk tisztítani, tudunk segíteni!</p>
             <p><small><a href="http://newsite.washember.hu/wp-content/uploads/2021/04/WE-web-discl-lak-2.pdf" target="blank">Adatvédelmi tájékoztató</a></small></p>
         </div>
     </section>
     <section id="form">
-        <p>Írj, miben segíthetünk</p>
+        <p>Írjon, miben segíthetünk</p>
         <div id="thankyou">
             <p>Köszönjük az üzenetet, hamarosan jelentkezünk!</p>
             <div class="button" id="tyButton">Újat írok</div>
         </div>
-        <?php echo do_shortcode('[contact-form-7 id="11" title="Üzenet"]'); ?>
+        <?php echo do_shortcode('[contact-form-7 id="25" title="Üzenet-lakossagi"]'); ?>
         <p><small>A munka felmérése megrendelés létrejötte esetén ingyenes, egyebekben kiszállási díjainkról tájékoztatást telefonos egyeztetés során adunk, melyet a kiszállásról szóló értesítő email-ben rögzítünk.</small></p>
     </section>
     <section id="contact">
-        <p>Ha sürgős, <u>hívj</u>, és <u>repülünk</u></p>
+        <p>Ha sürgős, <u>hívjon</u>, és <u>repülünk</u></p>
         <p><a href="tel:<?php the_field('telefonszam_lakossagi', 'option'); ?>"><?php the_field('telefonszam_lakossagi', 'option'); ?></a></p>
         <div class="copyright">
             <p><small>A weboldal nem tárol cookie-kat.</small></p>
--- dist/page-uzleti.php
+++ dist/page-uzleti.php
@@ -9,13 +9,7 @@ get_header();
 
 ?>
 
-<main class="grid grid--main">
-    <div id="bgImages">
-        <div class="wrapper">
-            <img src="<?php bloginfo('template_url') ?>/assets/img/img1.png" alt="" id="img1">
-            <img src="<?php bloginfo('template_url') ?>/assets/img/img2.png" alt="" id="img2">
-        </div>
-    </div>
+<main class="grid grid--main" id="content-uzleti">
     <section id="content">
         <div class="text">
             <div class="overflow-scroll" style="max-height: 600px;">
@@ -31,34 +25,34 @@ get_header();
                     eseti, prioritást igénylő feladatok elvégzése, akár hosszabb távú, szerződéses keretek között történő együttműködés
                     esetén.</p>
                     <p>Kompetenciáink, kollégáink:</p>
-                    <p>Tisztítás-technológiai és takarítási munkák, legyen szó akár lépcsőház takarításról, padlástér veszélyes hulladék
+                    <p><u>Tisztítás-technológiai és takarítási</u> munkák, legyen szó akár lépcsőház takarításról, padlástér veszélyes hulladék
                     kezeléséről, vagy fertőtlenítő takarításról.</p>
-                    <p>Karbantartó kollégáink hozzáértéssel és több év tapasztalattal felszerelkezve oldanak meg hétköznapi problémákat,
+                    <p><u>Karbantartó</u> kollégáink hozzáértéssel és több év tapasztalattal felszerelkezve oldanak meg hétköznapi problémákat,
                     meghibásodásokat.</p>
-                    <p>Ipari alpinos kollégáink rövid határidővel elérhetőek ereszcsatorna tisztításra, homlokzati veszélymentesítésre,
+                    <p><u>Ipari alpinos</u> kollégáink rövid határidővel elérhetőek ereszcsatorna tisztításra, homlokzati veszélymentesítésre,
                     cserépigazításra, régi antennák leszerelésére, és galamb-mentesítésre is.</p>
-                    <p>A takarítási szolgáltatás mellett kertrendezést, kertészeti munkákat is vállalunk.</p>
-                    <p>Partnercégeink bevonásával akár épület-gépészeti, villamossági és generál kivitelezési projektekben is
+                    <p>A takarítási szolgáltatás mellett <u>kertrendezést</u>, kertészeti munkákat is vállalunk.</p>
+                    <p><u>Partnercégeink</u> bevonásával akár <u>épület-gépészeti, villamossági és generál kivitelezési projektekben</u> is
                     segítségükre lehetünk.</p>
-                    <p>Hosszabb távú szerződéses keretek között történő együttműködés esetén jelentős árkedvezményt tudunk
+                    <p>Hosszabb távú <u>szerződéses</u> keretek között történő együttműködés esetén jelentős <u>árkedvezményt</u> tudunk
                     biztosítani ügyfeleink részére.</p>
-                    <p>Hívja a Washembert! Megbízható - Gyors - Precíz! Válassza Ön is a hosszútávú biztonságot!</p>
+                    <p>Hívja a <u>Washembert!</u> Megbízható - Gyors - Precíz! Válassza Ön is a hosszútávú biztonságot!</p>
                 </div>
             </div>
             <p class="policy"><small><a href="http://newsite.washember.hu/wp-content/uploads/2021/04/WE-web-discl-B2B-3.pdf" target="blank">Adatvédelmi tájékoztató</a></small></p>
         </div>
     </section>
     <section id="form">
-        <p>Írj, miben segíthetünk</p>
+        <p>Írjon, miben segíthetünk</p>
         <div id="thankyou">
             <p>Köszönjük az üzenetet, hamarosan jelentkezünk!</p>
             <div class="button" id="tyButton">Újat írok</div>
         </div>
-        <?php echo do_shortcode('[contact-form-7 id="11" title="Üzenet"]'); ?>
+        <?php echo do_shortcode('[contact-form-7 id="11" title="Üzenet-uzleti"]'); ?>
         <p><small>A munka felmérése megrendelés létrejötte esetén ingyenes, egyebekben kiszállási díjainkról tájékoztatást telefonos egyeztetés során adunk, melyet a kiszállásról szóló értesítő email-ben rögzítünk.</small></p>
     </section>
     <section id="contact">
-        <p>Ha sürgős, <u>hívj</u>, és <u>repülünk</u></p>
+        <p>Ha sürgős, <u>hívjon</u>, és <u>repülünk</u></p>
         <p><a href="tel:<?php the_field('telefonszam_uzleti', 'option'); ?>"><?php the_field('telefonszam_uzleti', 'option'); ?></a></p>
         <div class="copyright">
             <p><small>A weboldal nem tárol cookie-kat.</small></p>
